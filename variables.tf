0b50496b7451673bbd103948a8b899b452be4bc8
amitku13 <amitkumarbhontele@gmail.com>
2024-12-05 11:34:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "aws_region" {
   description = "The AWS region to deploy the EC2 instance."
   type        = string
-  default     = "us-east-1"
+  #default     = "us-east-1"
 }
 
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance."
   type        = string
-  default     = "ami-0453ec754f44f9a4a"
+  #default     = "ami-0453ec754f44f9a4a"
 }
 
 variable "instance_type" {
   description = "The instance type for the EC2 instance."
   type        = string
-  default     = "t2.micro"
+  #default     = "t2.micro"
 }
 
 variable "key_pair" {
   description = "The key pair name for SSH access."
   type        = string
-  default     = "mykeypair"
+  #default     = "mykeypair"
 }
