169a6887abc425ed593ecb00ae81766f3f2ffc9d
amitku13 <amitkumarbhontele@gmail.com>
2024-12-05 19:33:22 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,24 @@
-# Output for VPC ID
 output "vpc_id" {
-  value       = aws_vpc.main[0].id
+  value       = var.create_vpc ? aws_vpc.main[0].id : null
   description = "The ID of the created VPC (if created)."
-  condition   = var.create_vpc
 }
 
-# Output for Subnet ID
 output "subnet_id" {
-  value       = aws_subnet.main[0].id
+  value       = var.create_vpc ? aws_subnet.main[0].id : null
   description = "The ID of the created Subnet (if created)."
-  condition   = var.create_vpc
 }
 
-# Output for EC2 Instance ID
 output "ec2_instance_id" {
-  value       = aws_instance.main[0].id
+  value       = var.create_ec2 ? aws_instance.main[0].id : null
   description = "The ID of the created EC2 instance (if created)."
-  condition   = var.create_ec2
 }
 
-# Output for S3 Bucket Name
 output "s3_bucket_name" {
-  value       = aws_s3_bucket.main[0].bucket
+  value       = var.create_s3 ? aws_s3_bucket.main[0].bucket : null
   description = "The name of the created S3 bucket (if created)."
-  condition   = var.create_s3
 }
 
-# Output for Auto Scaling Group Name
 output "autoscaling_group_name" {
-  value       = aws_autoscaling_group.main[0].name
+  value       = var.create_autoscaling ? aws_autoscaling_group.main[0].name : null
   description = "The name of the created Auto Scaling group (if created)."
-  condition   = var.create_autoscaling
 }
