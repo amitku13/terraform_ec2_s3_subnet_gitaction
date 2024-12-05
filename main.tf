365d4436b67205143b99f7868728e9350856e39d
amitku13 <amitkumarbhontele@gmail.com>
2024-12-05 19:24:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,53 +2,70 @@ provider "aws" {
   region = var.aws_region
 }
 
-
+# VPC
 resource "aws_vpc" "main" {
   count      = var.create_vpc ? 1 : 0
   cidr_block = var.vpc_cidr
-  tags       = { Name = "Action-VPC" }
+  tags = {
+    Name = "Action-VPC"
+  }
 }
 
+# Subnet
 resource "aws_subnet" "main" {
   count             = var.create_vpc ? 1 : 0
   vpc_id            = aws_vpc.main[0].id
   cidr_block        = var.subnet_cidr
   availability_zone = var.availability_zone
-  tags              = { Name = "Action-Subnet" }
+  tags = {
+    Name = "Action-Subnet"
+  }
 }
 
+# EC2 Instance
 resource "aws_instance" "main" {
   count         = var.create_ec2 ? 1 : 0
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = var.create_vpc ? aws_subnet.main[0].id : var.subnet_id 
+  subnet_id     = var.create_vpc ? aws_subnet.main[0].id : var.subnet_id
   key_name      = var.key_pair_name
-  tags          = { Name = "Action-EC2" }
-}
-
-resource "random_id" "bucket_suffix" {
-  count = var.create_s3 ? 1 : 0
-  byte_length = 8
-}
-
-resource "aws_s3_bucket" "main" {
-  count  = var.create_s3 ? 1 : 0
-  bucket = "${var.s3_bucket_name}-${random_id.bucket_suffix[0].hex}"
-  tags   = { Name = "Action-S3" }
-}
-
-resource "aws_launch_template" "main" {
-  count          = var.create_autoscaling ? 1 : 0
-  name           = "asg-launch-template"
-  image_id       = var.ami_id
-  instance_type  = var.instance_type
-  key_name       = var.key_pair_name
-  tag_specifications {
-    resource_type = "instance"
-    tags          = { Name = "ASG-Instance" }
+  tags = {
+    Name = "Action-EC2"
   }
 }
 
+# Random S3 Bucket Suffix
+resource "random_id" "bucket_suffix" {
+  count       = var.create_s3 ? 1 : 0
+  byte_length = 8
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "main" {
+  count  = var.create_s3 ? 1 : 0
+  bucket = "${var.s3_bucket_name}-${random_id.bucket_suffix[0].hex}"
+  tags = {
+    Name = "Action-S3"
+  }
+}
+
+# Launch Template for Autoscaling
+resource "aws_launch_template" "main" {
+  count         = var.create_autoscaling ? 1 : 0
+  name          = "asg-launch-template"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_pair_name
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ASG-Instance"
+    }
+  }
+}
+
+# Autoscaling Group
 resource "aws_autoscaling_group" "main" {
   count             = var.create_autoscaling ? 1 : 0
   desired_capacity  = var.autoscaling_desired_capacity
